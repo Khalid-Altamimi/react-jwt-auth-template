329239a388b8f8fe76acf0010142ab699d127881
Khalid Altamimi <khalida.altamimi71@gmail.com>
2025-11-01 13:56:42 +0300

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
-import './App.css'
+// src/App.jsx
+
+import { Routes, Route } from 'react-router'; // Import React Router
+
+import NavBar from './components/NavBar/NavBar';
+// Import the SignUpForm component
+import SignUpForm from './components/SignUpForm/SignUpForm';
+import SignInForm from './components/SignInForm/SignInForm';
+import Landing from './components/Landing/Landing';
+import Dashboard from './components/Dashboard/Dashboard';
+import { useContext } from 'react';
+import { UserContext } from './contexts/UserContext';
 
 const App = () => {
+  const { user } = useContext(UserContext);
+
   return (
     <>
       <NavBar />
-      <h1>Hello, friend!</h1>
+
+      <Routes>
+        {
+          user ?
+          <>
+            <Route path='/' element={<Dashboard/>}/>
+            <Route path='/products' element={<h1>Producs</h1>}/>
+            <Route path='/favs' element={<h1>Favs</h1>}/>
+            <Route path='/profile' element={<h1>{user.username}</h1>}/>
+            <Route path='/orders' element={<h1>ORDERS</h1>}/>
+          </>
+            :
+            <Route path='/' element={<Landing/>}/>
+        }
+        <Route path='/sign-up' element={<SignUpForm />} />
+        <Route path='/sign-in' element={<SignInForm />} />
+      </Routes>
     </>
   );
 };
